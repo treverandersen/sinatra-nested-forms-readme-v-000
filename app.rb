df176e0e326486d3d8aff77d4dbd81f553eedd6d
treverandersen <trever.r.andersen@gmail.com>
2018-07-25 22:37:44 -0500

    iterate over course information

--- app.rb
+++ app.rb
@@ -9,6 +9,10 @@ class App < Sinatra::Base
 	post '/student' do 
 		@student = Student.new(params[:student])
 
+		params[:student][:courses].each do |details|
+			Course.new(details)
+		end
+
 		
 	end
 
